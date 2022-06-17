0b04f3c59eb9c7579a2e9399b458761c9d914321
Pranav Prashar <prasherlucky@gmail.com>
2022-06-17 18:14:28 -0400

    Dynamic conversation working, updated form to clear on submit

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -32,78 +32,27 @@ formElement.addEventListener("submit", function (event) {
 
   conversationFormInput(formName, formComment);
   //   createContainer(people);
+  formElement.reset();
 });
 
 function conversationFormInput(firstName, comment) {
   var dateObj = new Date();
-  var month = dateObj.getUTCMonth() + 1; //months from 1-12
+  var month = dateObj.getUTCMonth() + 1;
   var day = dateObj.getUTCDate();
   var year = dateObj.getUTCFullYear();
 
   let newdate = month + "/" + day + "/" + year;
   console.log(newdate);
 
-  //   let newObject = { firstName, newdate, comment };
-
-  createContainerTwo(firstName, newdate, comment);
-
-  // people.unshift({ firstName, newdate, comment });
   const obj = { name: firstName, date: newdate, conversation: comment };
-  // createContainer(obj);
+
+  removeAllChildNodes(conversationUsers);
+
   people.unshift(obj);
 
-  console.log(people);
-}
-
-// for (let i = 0; i < people.length; i++) {
-//   createContainer(people[i]);
-// }
-
-function createContainerTwo(firstName, date, converstation) {
-  //outer container
-  const conversationUsersContainer = document.createElement("div");
-  conversationUsersContainer.classList.add("conversation__users--container");
-
-  //image container
-  const conversationImgContainer = document.createElement("div");
-  conversationImgContainer.classList.add("conversation__users--img");
-
-  //Details container
-  const usersDetails = document.createElement("div");
-  usersDetails.classList.add("conversation__users--details");
-
-  //image
-  const conversationImg = document.createElement("div");
-  conversationImg.classList.add("conversation__img");
-  conversationImg.setAttribute("src", people.image);
-  conversationImgContainer.appendChild(conversationImg);
-
-  conversationUsersContainer.appendChild(conversationImgContainer); // Append the image container with the image
-
-  conversationUsersContainer.appendChild(usersDetails);
-
-  const detailsWrapper = document.createElement("div");
-  detailsWrapper.classList.add("details__wrapper");
-  usersDetails.appendChild(detailsWrapper);
-
-  const detailsWrapper__name = document.createElement("div");
-  detailsWrapper__name.innerHTML = firstName;
-  detailsWrapper__name.classList.add("details__wrapper--name");
-  detailsWrapper.appendChild(detailsWrapper__name);
-
-  const detailsWrapper__date = document.createElement("div");
-  detailsWrapper__date.innerHTML = date;
-  detailsWrapper__date.classList.add("details__wrapper--date");
-  detailsWrapper.appendChild(detailsWrapper__date);
-
-  const detailsParagraph = document.createElement("div");
-  detailsParagraph.innerHTML = converstation;
-  detailsParagraph.classList.add("details-paragraph");
-  usersDetails.appendChild(detailsParagraph);
-
-  conversationUsers.appendChild(conversationUsersContainer);
-  console.log(conversationUsersContainer);
-  //   console.log("Person:" + person);
+  for (let i = 0; i < people.length; i++) {
+    createContainer(people[i]);
+  }
 }
 
 function createContainer(person) {
@@ -157,41 +106,8 @@ for (let i = 0; i < people.length; i++) {
   createContainer(people[i]);
 }
 
-//  const conversationContainer = document.querySelector(".conversation");
-
-// function createCard(person){
-//     const conversationForm = document.createElement('div');
-//     conversationForm.classList.add('conversation__form');
-
-//     const cardContent = document.createElement("div");
-//     cardContent.classList.add("card__content");
-//     conversationForm.appendChild(cardContent);
-
-//     const usersDetails = document.createElement("div");
-//     usersDetails.classList.add("conversation__users--details");
-//     cardContent.appendChild(usersDetails);
-
-//     const detailsWrapper = document.createElement("div");
-//     detailsWrapper.classList.add("details__wrapper");
-//     usersDetails.appendChild(detailsWrapper);
-
-//     const detailsWrapper__name = document.createElement("div");
-//     detailsWrapper__name.innerHTML = person.name;
-//     detailsWrapper__name.classList.add("details__wrapper--name");
-//     detailsWrapper.appendChild(detailsWrapper__name);
-
-//     const detailsWrapper__date = document.createElement("div");
-//     detailsWrapper__date.innerHTML = person.date;
-//     detailsWrapper__date.classList.add("details__wrapper--date");
-//     detailsWrapper.appendChild(detailsWrapper__date);
-
-//     const detailsParagraph = document.createElement("div");
-//     detailsParagraph.innerHTML = person.conversation;
-//     detailsParagraph.classList.add("details-paragraph");
-//     usersDetails.appendChild(detailsParagraph);
-
-//     conversationContainer.appendChild(conversationForm);
-
-//     console.log(conversationForm);
-
-// }
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
